fc70acc473261937d1201e0b078484784dd6dde3
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-07-20 10:26:02 -0300

    Exercício: programa com Exception e RuntimeException

--- src/application/ProgramAccount.java
+++ src/application/ProgramAccount.java
@@ -8,4 +8,33 @@ import java.util.Scanner;
 
 public class ProgramAccount {
 
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Digite os dados da conta: ");
+            System.out.print("Número: ");
+            int number = sc.nextInt();
+            System.out.print("Nome do Titular: ");
+            sc.nextLine();
+            String holder = sc.nextLine();
+            System.out.print("Saldo inicial: ");
+            double balance = sc.nextDouble();
+            System.out.print("Limite de saque: ");
+            double withdrawLimit = sc.nextDouble();
+            Account account = new Account(number, holder, balance, withdrawLimit);
+
+            System.out.println();
+            System.out.print("Digite o valor para saque: ");
+            double amount = sc.nextDouble();
+            account.withdraw(amount);
+            System.out.println("Novo saldo: " + account.getBalance());
+        } catch (DomainException e) {
+            System.out.println("Erro de saque: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println("Erro inesperado!");
+        }
+        sc.close();
+    }
 }
