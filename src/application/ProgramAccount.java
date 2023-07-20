b44e70005f4cc9400d39741c757497310197a3b1
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-07-20 10:23:55 -0300

    Exercício Tratamento de exceções: implementando entidade

---
+++ src/application/ProgramAccount.java
@@ -0,0 +1,11 @@
+package application;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramAccount {
+
+}
